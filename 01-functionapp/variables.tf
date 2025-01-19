15623a82fe42d3267bcead14cdce9a44e004308b
Mike Monaco <mamonaco1973@gmail.com>
2025-01-19 18:04:42 -0500

    Clean up code for demo

--- 01-functionapp/variables.tf
+++ 01-functionapp/variables.tf
@@ -1,5 +1,5 @@
 variable "authorization_level" {
-  description = "What type of authorization to require on the functions"
+  description = "What type of authorization to require on the function"
   type        =  string
   default     = "ANONYMOUS" 
 #  default     = "FUNCTION"
